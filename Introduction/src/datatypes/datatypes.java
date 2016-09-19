25bc560cd14326e416f2812b684378c1896eac78
Student6 <Student6@10.8.33.148>
2016-09-19 13:04:08 -0400

    ok

---
+++ Introduction/src/datatypes/datatypes.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5.0/2);
+		System.out.println((double) 5/2);
+		System.out.println(5/2);
+	}
+
+}
