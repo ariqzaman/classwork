2280640d1d0b164db3e87782399307bec0f267c7
Student6 <Student6@10.8.33.148>
2016-09-22 12:34:02 -0400

    xd

--- Introduction/src/datatypes/StringPractice.java
+++ Introduction/src/datatypes/StringPractice.java
@@ -1,8 +1,50 @@
 package datatypes;
 
-public class StringPractice {
+import java.util.Scanner;
 
+public class StringPractice {
+	
+	static Scanner input;
+	static String user;
+	static int lineCount;
+	
 	public static void main(String[] args) {
+		createAScanner();
+		lineCount = 0;
+		demonstrateStringMethods();
+		promptName();
+		talkForever();
+	}
+	private static void promptName() {
+		print("hello human blah blah blach wats ur name?");
+		user = input.nextLine();
+		print("hello "+ user);
+		
+	}
+	public static void talkForever(){
+		while(true){
+			promptInput();
+		}
+	}
+	
+	private static void promptInput() {
+		print(user +" try putting in a string");
+		String userInput = input.nextLine();
+		print("you typed" + userInput);
+		
+	}
+	
+	public static void print(String s){
+		lineCount++;
+		System.out.println("Line # "+ lineCount+ ": " + s);
+	}
+
+	public static void createAScanner() {
+		input = new Scanner(System.in);
+	}
+	
+	
+	public static void demonstrateStringMethods(){
 		//String text = new String ("hello world");
 		String text = "hello world";// same as above
 		String text2 = "hello ";
@@ -16,7 +58,6 @@ public class StringPractice {
 			if (word1.compareTo(word2) < 0){
 				System.out.println("word1 is b4 word2");
 			}
-
 	}
 
 }
