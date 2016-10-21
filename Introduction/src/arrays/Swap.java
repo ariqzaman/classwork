1a57ba166e0adee648f709efe93f9da8e920e9e8
Student6 <Student6@10.8.33.148>
2016-10-21 13:09:48 -0400

    Swap and half

---
+++ Introduction/src/arrays/Swap.java
@@ -0,0 +1,17 @@
+package arrays;
+import java.util.Arrays;
+
+public class Swap {
+
+	public static void main(String[] args) {
+
+		int[] arr = {2, 3, 4, 5, 6, 7, 12, 15, 16};
+		//swaps first n last
+		swap(arr, 0, arr.length-1);
+	}
+	private static void swap(int[] arr, int i, int j){
+		int placeholder = arr[i];
+				arr[j]=arr[i];
+				arr[i] = placeholder;
+	}
+}
