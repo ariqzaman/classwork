3371393b2c7f466b44bcd909df3804f417f318cc
Student6 <Student6@10.8.33.148>
2016-11-07 12:34:36 -0500

    th

--- Introduction/src/CaveExplorer/TwoDArrayIntro.java
+++ Introduction/src/CaveExplorer/TwoDArrayIntro.java
@@ -1,147 +1,56 @@
 package CaveExplorer;
 
+import java.util.Scanner;
+
 public class TwoDArrayIntro {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
-		boolean[][] mines = new boolean[6][6];
-		createMines(mines, 10);
-		String[][] field = 
-				new String[mines.length][mines[0].length];
-		matchValues(field, mines);
-		printPic(field);
-	
+		scanner = new Scanner(System.in);
+
+		int roomsY = 10, roomsX = 10;
+		int x = 0, y = 0;
+
+		while (true) {
+			System.out.println("You are in room " + x + " " + y + ". What do u want to do? Enter:");
+			System.out.println("w, a, s, d");
+			print(roomsY, roomsX, y, x);
+			String input = scanner.nextLine();
+
+			if (input.equals("w") && y + 1 < roomsY) {
+				y++;
+			} else if (input.equals("a") && x > 0) {
+				x--;
+			} else if (input.equals("s") && y > 0) {
+				y--;
+			} else if (input.equals("d") && x + 1 < roomsX) {
+				x++;
+			}
+		}
 	}
-	
-	
-	private static void matchValues(String[][] field, boolean[][] mines) {
-		for(int row = 0; row < field.length; row++){
-			for(int col = 0; col < field[0].length; col++){
-				if(mines[row][col])field[row][col]="X";
-				else{
-					field[row][col] = 
-							countAdjacent(mines, row, col);
+
+	public static void print(int roomsX, int roomsY, int posX, int posY) {
+		for (int j = 0; j != roomsX; j++) {
+			System.out.print("____");
+		}
+		System.out.println();
+
+		for (int i = 0; i != roomsY; i++) {
+			for (int j = 3; j > 0; j--) {
+				for (int k = 0; k != roomsX; k++) {
+					if (j == 1) {
+						System.out.print("|___");
+					} else {
+						if (j == 2 && i == posX && k == posY) {
+							System.out.print("| X ");
+						} else {
+							System.out.print("|   ");
+						}
+					}
 				}
+				System.out.println("|");
 			}
-		}
-	}
-
-
-	private static String countAdjacent(boolean[][] mines, 
-			int r, int c) {
-//		//r and c represent coordinates of element 
-//		//we are providing a 
-//		//String for
-		int count = 0;
-//		//loop through row above to row below
-//		for(int row = r-1; row <= r+1; row++){
-//			//loop through col left to col right
-//			for(int col = c-1; col <= c+1; col++){
-//				//exclude this element when counting
-//				if(row !=r && col != c){
-//					if(row >=0 &&
-//							row < mines.length&&
-//							col >= 0 && 
-//							col < mines[row].length) {
-//						
-//					}
-//				}
-//			}
-//		}
-//		return null;
-		
-		//specific method
-		//up
-		count+= validAndTrue(mines, r-1, c);
-		//below
-		count+= validAndTrue(mines, r+1, c);
-		//right
-		count+= validAndTrue(mines, r, c+1);
-		//left
-		count+= validAndTrue(mines, r, c-1);
-		
-		return count+"";
-	}
-
-
-	private static int validAndTrue(boolean[][] mines, int i, int j) {
-		if(i>=0 && i<mines.length && j>=0 && j<mines[0].length && mines[i][j]){
-			return 1;
-		}else{
-		return 0;
-		}
-	}
-
-
-	private static void createMines(boolean[][] mines, 
-			int numberOfMines) {
-		while(numberOfMines >0){
-			int row = (int)(Math.random()*mines.length);
-			int col = (int)(Math.random()*mines[0].length);
-			if(!mines[row][col]){
-				mines[row][col] = true;
-				numberOfMines --;
-			}
-		}
-		
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	public static void picDrawing(){
-		String[][] pic = new String[10][12];
-		for(int row = 0; row< pic.length; row++){
-			for(int col = 0; col < pic[row].length; col++){
-				pic[row][col] = " ";
-			}
-		}
-		pic[1][2]="O";
-		pic[0][2]="|";
-		pic[2][2]="|";
-		pic[1][1]="-";
-		pic[1][3]="-";
-		pic[0][1]="\\";
-		pic[2][3]="\\";
-		pic[0][3]="/";
-		pic[2][1]="/";		
-		
-		for(int row = 7; row< pic.length; row++){
-			for(int col = 0; col < pic[row].length; col++){
-				pic[row][col] = "M";
-			}
-		}
-		//top & bottom row is "_"
-		for(int col = 0 ; col < pic[0].length; col++){
-			pic[0][col]= "_";
-			pic[pic.length-1][col]= "_";
-		}
-		//left and right col is "|"
-		for(int row = 1; row < pic.length; row++){
-			pic[row][0]="|";
-			pic[row][pic[0].length-1]="|";
-		}
-
-	}
-	
-	public static void printPic(String[][] pic){
-		for(int row = 0; row< pic.length; row++){
-			for(int col = 0; col < pic[row].length; col++){
-				System.out.print(pic[row][col]);
-			}
-			System.out.println();
 		}
 	}
 
--- Introduction/src/arrays/TwoDimentionalArray.java
+++ Introduction/src/arrays/TwoDimentionalArray.java
@@ -14,22 +14,22 @@ public class TwoDimentionalArray {
 	public static void main(String[] args) {
 		
 		
-		pic = new String[5][4];//String[a][b] a= length/rows b= length of each array/col
-		arr2D = new String[5][4];
+		pic = new String[6][8];//String[a][b] a= length/rows b= length of each array/col
+		arr2D = new String[6][8];
 		
 		for(int row = 0; row < arr2D.length; row++)
 		{
 			//populate with coordinates
 			for(int col = 0; col<arr2D[row].length;col++)
 			{
-				arr2D[row][col] = "(" + row + "," + col + ")";
+				arr2D[row][col] = " ";
 					
 			}
 		}
 		
 		//Styart positions
-		i = 2;
-		j = 3;
+//		i = 2;
+//		j = 3;
 		
 		in = new Scanner(System.in);
 		startExploring();
@@ -39,12 +39,11 @@ public class TwoDimentionalArray {
 
 	private static void startExploring() {
 			while(true){
-				printPic(pic);
-				System.out.println("you are in room"+ arr2D[i][j]);
-				System.out.println("what do u want to do?");
+				printPic(arr2D);
+				System.out.println("choose a column between 0-7");
 				String input = in.nextLine();
 				while(!isValid(input)){
-					System.out.println("pls enter w a s d");
+					System.out.println("pls enter a valid input between 0-7.");
 					input = in.nextLine();
 				}
 				interpretInput(input);
@@ -54,41 +53,69 @@ public class TwoDimentionalArray {
 	}
 
 
+//	public static void printPic(String[][] pic){
+//		for(int row = 0; row< pic.length; row++){
+//			for(int col = 0; col < pic[row].length; col++){
+//				if(row == i && col == j){
+//					System.out.print("x");
+//				} else if (pic[row][col] == null) {
+//					System.out.print(".");
+//				}
+//			}
+//			System.out.println();
+//		}
+//	}
+	
 	public static void printPic(String[][] pic){
 		for(int row = 0; row< pic.length; row++){
 			for(int col = 0; col < pic[row].length; col++){
-				if(row == i && col == j){
-					System.out.print("x");
-				} else if (pic[row][col] == null) {
-					System.out.print(".");
-				}
+				System.out.print(pic[row][col]);
 			}
 			System.out.println();
 		}
 	}
 
 
+//	private static void interpretInput(String input) {
+//		int iOrig = i;
+//		int jOrig = j;
+//		if(input.equals("w")&& i>0)i--;
+//		else if(input.equals("a")&& j>0)j--;
+//		else if(input.equals("d")&& j+1<arr2D[0].length)j++;
+//		else if(input.equals("s")&& i+1<arr2D.length)i++;
+//		if(iOrig == i && jOrig == j){
+//			System.out.println("u r at the edge of the universe. u can no lo"
+//					+ "nger to mvoe int hat direction");
+//		}
+//	}
+
 	private static void interpretInput(String input) {
-		int iOrig = i;
-		int jOrig = j;
-		if(input.equals("w")&& i>0)i--;
-		else if(input.equals("a")&& j>0)j--;
-		else if(input.equals("d")&& j+1<arr2D[0].length)j++;
-		else if(input.equals("s")&& i+1<arr2D.length)i++;
-		if(iOrig == i && jOrig == j){
-			System.out.println("u r at the edge of the universe. u can no lo"
-					+ "nger to mvoe int hat direction");
+		int test = Integer.parseInt(input);
+			for(int i = arr2D.length-1; i>-1; i--){
+				if(!(arr2D[i][test].equals("o"))){
+					arr2D[i][test] = "o";
+					break;
+				}
+				if(arr2D[0][test].equals("o")){
+			System.out.println("this column is full choose another column.");
+			}
+				}
 		}
-	}
-
-
+//		if(input.equals("0")&& i>0)i--;
+//		else if(input.equals("1")&& j>0)j--;
+//		else if(input.equals("2")&& j+1<arr2D[0].length)j++;
+//		else if(input.equals("3")&& i+1<arr2D.length)i++;
+	
+	
+	
 	private static boolean isValid(String input) {
-		String lc = input.toLowerCase();
-		String[] keys = {"w","a","s","d"};
+//		String lc = input.toLowerCase();
+		String[] keys = {"0","1","2","3","4","5","6","7"};
 		for(String key:keys){
-			if(key.equals(lc))return true;
+			if(key.equals(input))return true;
 		}
 		return false;
 	}
+
 
 }
